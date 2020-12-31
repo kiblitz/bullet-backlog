569c72a02c439ab43326c93c5827b924f1dbbb6d
thisistrivial <g.lee13770334@gmail.com>
2020-12-31 10:45:54 -0500

    Added errors

--- src/errors.py
+++ src/errors.py
@@ -1,25 +1,30 @@
 
+def bullet_exists():
+  return __err_format('Bullet already exists')
+
 def no_tags():
-  pass
+  return __err_format('No tags specified')
 
 def no_task():
-  pass
+  return __err_format('No task id specified')
 
 def task_not_found(task_id):
-  pass
+  return __err_format('Unrecognized task id <%s>' % task_id)
 
 def self_relative():
-  pass
+  return __err_format('Cannot relate task with itself')
 
 def no_bullet():
-  pass
+  return __err_format('No bullet found')
 
 def invalid_status(code):
-  pass
+  return __err_format('Invalid status <%s>' % code)
 
 def invalid_level(code):
-  pass
+  return __err_format('Invalid level <%s>' % code)
 
 def no_attribute():
-  pass
+  return __err_format('No attribute specified')
 
+def __err_format(error):
+  return 'Error: %s' % error
